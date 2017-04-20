c4351c40cefc733771c0d8c457d10968ce3d1902
Sergey Kovtunenko <skovtunenko@exadel.com>
2017-04-20 22:50:32 +0300

    Added initial structure for basic maven Kotlin 1.1.1/Java 8 project.

---
+++ src/main/kotlin/com/skovtunenko/pet/DemoApplication.kt
@@ -0,0 +1,8 @@
+package com.skovtunenko.pet
+
+/**
+ * Main entrance
+ */
+fun main(args: Array<String>) {
+    println("Hello world!")
+}
---
+++ src/test/kotlin/com/skovtunenko/pet/DemoApplicationTests.kt
@@ -0,0 +1,14 @@
+package com.skovtunenko.pet
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.BlockJUnit4ClassRunner
+
+@RunWith(BlockJUnit4ClassRunner::class)
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
